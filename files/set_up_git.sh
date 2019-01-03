c30035623e55635cab6dca86efb4a5fa1c56e641
Seb Jones <jonesseb100@gmail.com>
2019-01-03 14:13:25 +0000

    Modified setup script to set remotes correctly

--- files/set_up_git.sh
+++ files/set_up_git.sh
@@ -1,2 +1,5 @@
-cd ..
-git clone git@github.com:HPTPC/instrumentation_paper.git git_instrumentation_paper
+git clone https://git.overleaf.com/5be9b39667be113d97d7b7f0 hptpc_flux_paper
+cd hptpc_flux_paper
+# Set up origin
+git remote set-url --add --push origin https://git@github.com/HPTPC/HPTPCBeamFluxPaper.git
+git remote set-url --add --push origin https://git.overleaf.com/5be9b39667be113d97d7b7f0
